f8fd15312a7b0b2c05bf1fe6fa46c062e4dc598b
Tom Houlé <tom@tomhoule.com>
2021-09-29 11:24:41 +0200

    wip

--- prisma-fmt/builder.sh
+++ prisma-fmt/builder.sh
@@ -1,5 +1,7 @@
+export PATH="$rust/bin:$coreutils/bin"
+
 echo "Building WASM from source..."
-cargo build --lib --target=wasm32-unknown-unknown --target-dir=wasm-target
+cargo build --manifest-path=$src/prisma-fmt/Cargo.toml --lib --target=wasm32-unknown-unknown --target-dir=wasm-target
 echo "Building JS module from WASM..."
-wasm-bindgen --target=nodejs ./wasm-target/wasm32-unknown-unknown/debug/prisma_fmt.wasm --out-dir=js-target
+wasm-bindgen --target=nodejs ./wasm-target/wasm32-unknown-unknown/debug/prisma_fmt.wasm --out-dir=$out
 echo "ok"
--- prisma-fmt/flake.nix
+++ prisma-fmt/flake.nix
@@ -2,26 +2,56 @@
   description = "WASM prisma-fmt";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    wasm-bindgen = {
+      url =
+        "https://github.com/rustwasm/wasm-bindgen/archive/refs/tags/0.2.78.tar.gz";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, wasm-bindgen }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
         bash = pkgs.bash;
         rust = pkgs.rust-bin.stable.latest.default;
+        coreutils = pkgs.coreutils;
+        wasm_bindgen = pkgs.wasm-bindgen-cli;
+        glibc = pkgs.glibc;
       in {
-        nixpkgs.overlays = [ rust-overlay.overlay ];
-        defaultPackage = derivation {
+        defaultPackage = pkgs.rustPlatform.buildRustPackage rec {
+          buildInputs = [ actualHello which ];
           name = "prisma-fmt-wasm";
-          builder = "${bash}/bin/bash";
-          args = [ ./builder.sh ];
-          system = system;
-
-          inherit rust;
+          src = ./.;
+          cargoSha256 = pkgs.lib.fakeHash;
+          target = "wasm32-unknown-unknown";
+          cargoBuildFlags = [ "-p" "prisma-fmt" ];
+          # cargoLock = {
+          #   outputHashes = {
+          #     # "barrel-0.6.6-alpha.0" = pkgs.lib.fakeHash;
+          #     # "cuid-0.1.0" = pkgs.lib.fakeHash;
+          #     # "graphql-parser-0.3.0" = pkgs.lib.fakeHash;
+          #     # "mysql_async-0.27.0" = pkgs.lib.fakeHash;
+          #     # "postgres-native-tls-0.5.0" = pkgs.lib.fakeHash;
+          #     # "quaint-0.2.0-alpha.13" = pkgs.lib.fakeHash;
+          #     # "tokio-native-tls-0.3.0" = pkgs.lib.fakeHash;
+          #   };
+          # };
         };
-        devShell = import ./shell.nix { inherit pkgs; };
+        # derivation {
+        # name = "prisma-fmt-wasm";
+        # builder = "${bash}/bin/bash";
+        # args = [ ./builder.sh ];
+        # system = system;
+        # src = ../.;
+
+        #   inherit rust coreutils wasm_bindgen;
+        # };
+
+        devShell = pkgs.mkShell { buildInputs = [ rust ]; };
       });
 }
